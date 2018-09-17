b98cf804f3fb523953d2f8feb412dd14f92c7f95
KwonDae <goodday94@cnu.ac.kr>
2018-09-18 00:44:01 +0900

    make android folder

---
+++ CNUSignal/CNUSignal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CNUSignal
+//
+//  Created by Jinwoo on 17/09/2018.
+//  Copyright © 2018 201402343. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
