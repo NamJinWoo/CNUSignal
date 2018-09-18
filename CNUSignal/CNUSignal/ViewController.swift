f7af37fa5f09ec99a4114b2fe3693a71ab46537a
NamJinWoo <cjsencks@naver.com>
2018-09-18 22:15:14 +0900

    comment

--- CNUSignal/CNUSignal/ViewController.swift
+++ CNUSignal/CNUSignal/ViewController.swift
@@ -25,21 +25,19 @@ class ViewController: UIViewController {
         
     }
     @IBAction func loginBtn(_ sender: Any) {
+        //로그인 버튼을 눌렀을 때.
         Auth.auth().signIn(withEmail: idTextField.text!, password: passwordTextField.text!) { (user, error) in
             if user != nil{
-                
                 print("login success")
-                
             }
                 
             else{
-                
                 print("login fail")
-                
             }
         }
     }
     @IBAction func signupBtn(_ sender: Any) {
+        //회원가입 버튼을 눌렀을때. 뷰컨트롤러 바뀌어야함
     }
 
 }
